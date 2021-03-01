93161d98fe912ad7a240587d29c4c4c38f03733c
Dawud Rehan <d.rehan7@gmail.com>
2021-03-01 15:21:56 -0500

    foreign key working; artist and album model created

--- django-music-collab/core/models.py
+++ django-music-collab/core/models.py
@@ -6,10 +6,20 @@ class User(AbstractUser):
     pass
 
 
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    genre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} Born on:{self.birthday} Genre:{self.genre}"
+
+
 class Album(models.Model):
     title = models.CharField(max_length=100)
-    artist = models.CharField(max_length=100)
+    artist = models.ForeignKey(
+        Artist, max_length=100, on_delete=models.CASCADE)
     song_year = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.title} | {self.artist} {str(self.song_year)}"
+        return f"{self.title} {self.song_year}{self.artist}"
