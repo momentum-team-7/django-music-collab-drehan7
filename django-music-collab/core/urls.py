44d85452f310ef86867d82439d2ebec5a568158a
Dawud Rehan <d.rehan7@gmail.com>
2021-03-02 15:11:36 -0500

    album page and artist page rendered. working on pictures; checkpoint

--- django-music-collab/core/urls.py
+++ django-music-collab/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('artist/<int:pk>/', views.artist_info, name='artist_info'),
+    path('artists/', views.artists, name="artists"),
+    path('albums', views.albums, name="albums"),
+    path('album/<int:pk>/', views.album_info, name="album_info"),
 ]
--- django-music-collab/core/views.py
+++ django-music-collab/core/views.py
@@ -9,6 +9,21 @@ def index(request):
     return render(request, 'index.html', {'albums': albums})
 
 
+def artists(request):
+    artists = Artist.objects.all()
+    return render(request, 'artists.html', {'artists': artists})
+
+
+def albums(request):
+    albums = Album.objects.all()
+    return render(request, 'albums.html', {'albums': albums})
+
+
 def artist_info(request, pk):
     artist = get_object_or_404(Artist, pk=pk)
     return render(request, 'artist_info.html', {'artist': artist})
+
+
+def album_info(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'album_info.html', {'album': album})
