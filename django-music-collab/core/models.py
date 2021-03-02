799e7a1304710ba028a9eacc9ee79828760a89d9
Dawud Rehan <d.rehan7@gmail.com>
2021-03-02 15:40:52 -0500

    album photos rendered

--- django-music-collab/core/models.py
+++ django-music-collab/core/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
 
 class Artist(models.Model):
     name = models.CharField(max_length=50)
+    picture = models.CharField(max_length=250, blank=True, null=True)
     birthday = models.DateField()
     genre = models.CharField(max_length=20)
 
@@ -19,7 +20,8 @@ class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(
         Artist, max_length=100, on_delete=models.CASCADE)
-    song_year = models.IntegerField(blank=True, null=True)
+    album_year = models.IntegerField(blank=True, null=True)
+    album_photo = models.CharField(max_length=250, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.title} {self.song_year}{self.artist}"
+        return f"{self.title} {self.album_year}{self.artist}"
